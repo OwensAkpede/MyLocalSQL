5c27a14020912ec3d82e67798cffaba37e6f0a5d
Johnsonmach6@gmail.com <Johnsonmach6@gmail.com>
2023-10-11 16:29:40 +0100

    localSQL

--- index.php
+++ index.php
@@ -1,29 +1,16 @@
 <?php
+
 require_once("./localSQL.php");
-//$name = random_int(1, 1000000);
+
+$name = random_int(1, 1000000);
 
 
 $mysql = new LocalSQL("localhost", "root", "");
+
 $localStorage = $mysql->localStorage("my store");
 
-//$localStorage->setItem($name, "hello world");
+$localStorage->setItem($name, "hello world");
+
 print_r($localStorage->keys());
 
-
-/***
- * 
- * 
- * 
- * 
- * require_once("./localSQL.php");
-
-$mysql = new Database("localhost", "root", "");
-$db = $mysql->openDB("my db");
-$localStorage = $db->openTable("my table");
-$localStorage->setItem("name", "john");
-$name = $localStorage->getItem("name");
- * 
- * 
- * 
- */
 ?>
