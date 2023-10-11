016d6300790af9f0ea673f28359f6a71539a2e40
Johnsonmach6@gmail.com <Johnsonmach6@gmail.com>
2023-10-11 20:33:20 +0100

    updated

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 require_once("./localSQL.php");
 
 $name = random_int(1, 1000000);
-$name = "item";
 
 
 $mysql = new LocalSQL("localhost", "root", "");
 
 $localStorage = $mysql->localStorage("my store");
 
-$localStorage->setItem($name, "hello . world");
+$localStorage->setItem($name, "hello world");
 
-print_r($localStorage->getItem($name));
-print_r($localStorage);
+print_r($localStorage->keys());
 
 ?>
